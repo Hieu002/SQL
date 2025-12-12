5a91d74ef9b6fa6f01f10ff83e48d4749877e6a6
Hieu002 <dachieu8a@gmail.com>
2025-12-12 22:36:55 +0700

    update

--- Bai1/testing_system_assignment_1.sql
+++ Bai1/testing_system_assignment_1.sql
@@ -4,7 +4,7 @@ use testing_system_assignment_1;
 
 create table department (
 	department_id int auto_increment primary key,
-    department_name varchar(100)
+    department_name varchar(100) not null
     );
 
 create table position  (
@@ -43,3 +43,48 @@ create table type_question (
 	type_id int auto_increment primary key,
 	type_name varchar(50)
     );	
+
+create table category_question (
+	category_id int auto_increment primary key,
+    category_name varchar(50)
+    );
+    
+create table question (
+	question_id int auto_increment primary key,
+    content text,
+    category_id int,
+    type_id int,
+    creator_id int,
+    create_date date,
+    foreign key (category_id) references category_question(category_id),
+    foreign key (type_id) references type_question (type_id),
+    foreign key (creator_id) references GroupTable (creator_id)
+    );
+
+create table answer (
+	answer_id int auto_increment primary key,
+    content text,
+    question_id int,
+    isCorrect boolean,
+    foreign key (question_id) references question (question_id)
+    );
+
+create table exam (
+	exam_id int auto_increment primary key,
+    code varchar(50),
+    title varchar(100),
+    category_id int,
+    duration int,
+    creator_id int,
+    create_date date,
+    foreign key (category_id) references category_question (category_id),
+    foreign key (creator_id) references GroupTable (creator_id)
+    );
+
+create table exam_question (
+	exam_id int,
+    question_id int,
+    primary key (exam_id, question_id),
+    foreign key (exam_id) references exam (exam_id),
+    foreign key (question_id) references question (question_id)
+    );
